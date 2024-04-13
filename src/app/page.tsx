7c6508606facb2384ffae160abeba65ba7c85e21
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-04-13 10:34:23 +0000

    Add Shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-
+import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className='text-3xl font-bold bg-green-400'>Ashutosh Gaurav</h1>
+      <Button>Click me</Button>
     </main>
   )
 }
